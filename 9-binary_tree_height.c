ad91c045d9c68604dfa8946fefa5dcfffc1c1aaf
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-28 02:05:13 -0800

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_height - function to calculate tree height
+ * @tree: the current tree
+ *
+ * Return: returns size of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+	{
+		return -1; /* Height of empty tree */
+	}
+	else
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+		return ((left_height > right_height ? left_height : right_height) + 1);
+	}
+}
