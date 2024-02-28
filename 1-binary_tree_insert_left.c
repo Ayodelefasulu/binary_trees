a61a0a11eb3fd099ec20e99876599a173c6c3706
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-27 23:06:41 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,23 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	parent->left = binary_tree_node(parent, value);
-	return (parent->left);
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+		return NULL;
+	newNode = malloc(sizeof(binary_tree_t));
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
+
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
+	/* Set the left child of parent to the new node */
+	parent->left = newNode;
+
+	return newNode;
 }
