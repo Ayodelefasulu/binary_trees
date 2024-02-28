fc29f178233d067f8c513dfbdd0a8d7aed140cb3
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-28 07:13:06 -0800

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,22 +2,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leave - Counts the number of leaves in a binary tree
+ * binary_tree_leaves - Counts the number of leaves in a binary tree
  * @tree: Pointer to the node to measure the depth
  *
  * Return: Number of leaves in the tree
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
+	size_t i = 0;
 
 	if (tree == NULL)
-                return (0);
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		count++;
+		i++;
 	}
-	return (count + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
-        //return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (i + binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
