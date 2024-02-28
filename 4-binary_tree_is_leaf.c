1a46c64a966f452f735cbc58a59ec8bdfb690edc
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-28 00:03:19 -0800

    task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - checks if node is leaf
+ * @node: node to check
+ *
+ * Return: returns 1 or 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return -1;
+
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
