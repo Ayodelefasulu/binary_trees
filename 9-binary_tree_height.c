66d58b15c0fd5642f179c36d48775dbdd8d3f725
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-28 02:23:31 -0800

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,18 +2,6 @@
 #include "binary_trees.h"
 
 /**
- * max - Returns the maximum of two numbers
- * @a: First number
- * @b: Second number
- *
- * Return: The maximum of a and b
- */
-size_t max(size_t left_height, size_t right_height)
-{
-	return (left_height > right_height ? left_height : right_height);
-}
-
-/**
  * binary_tree_height - function to calculate tree height
  * @tree: the current tree
  *
@@ -33,6 +21,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		left_height = binary_tree_height(tree->left);
 		right_height = binary_tree_height(tree->right);
-		return 1 + max(left_height, right_height);
+		return ((left_height > right_height ? left_height : right_height) + 1);
 	}
 }
