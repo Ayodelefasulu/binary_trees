5a4cd26dd9099c5e1ec8d7464f7814e54837c53e
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-28 18:13:27 -0800

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_nodes - Counts the number of leaves (at least 1)in a binary tree
+ * @tree: Pointer to the node to measure the depth
+ *
+ * Return: Number of leaves in the tree
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if ((tree->left != NULL || tree->right != NULL) || (tree->left != NULL && tree->right != NULL))
+	if ((tree->left != NULL || tree->right != NULL) ||
+		(tree->left != NULL && tree->right != NULL))
 	{
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
